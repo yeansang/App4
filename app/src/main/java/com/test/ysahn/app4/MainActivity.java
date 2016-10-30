dd39d3e4c771a620bab6292922b949a238ed2421
yeansang an <ysan1991@gmail.com>
2016-10-31 00:13:00 +0900

    finish

--- app/src/main/java/com/test/ysahn/app4/MainActivity.java
+++ app/src/main/java/com/test/ysahn/app4/MainActivity.java
@@ -1,23 +1,52 @@
 package com.test.ysahn.app4;
 
+import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private WebView mWebview;
+    private Button mbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
         setContentView(R.layout.activity_main);
 
         mWebview = (WebView) findViewById(R.id.webview);
+        mbutton = (Button) findViewById(R.id.button);
 
         mWebview.setWebViewClient(new WebViewClient());
-        mWebview.loadUrl("http://m.daum.net");
+        mWebview.getSettings().setJavaScriptEnabled(true);
+        mWebview.getSettings().setUserAgentString("Mozila ");
+        mWebview.loadUrl("http://www.newsis.com/ar_detail/view.html?ar_id=NISX20161026_0014476117&cID=10201&pID=10200");
 
+        mWebview.addJavascriptInterface(new WebAppInterface(this), "test");
+        mbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mWebview.loadUrl("javascript:document.location='http://www.google.co.uk/gwt/n?u='+document.location");
+            }
+        });
+
+    }
+    public class WebAppInterface {
+        Context mContext;
+        /** Instantiate the interface and set the context */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+        /** Show a toast from the web page */
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
     }
 }
